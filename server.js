141d9d1138eb0152b675618162a199359d4e07ce
Tiana <wiggins.tiana@gmail.com>
2019-02-23 23:36:39 -0500

    routing changes

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/app/css"));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("/Users/josephine-maye/Desktop/FriendFinder/app/routing/apiRoutes")(app);
+require("/Users/josephine-maye/Desktop/FriendFinder/app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
