e4070a85d1925315a17a3a3af75a4af8d7c9e715
TianaW43311 <42350958+TianaW43311@users.noreply.github.com>
2019-02-23 22:56:05 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/app/css"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
