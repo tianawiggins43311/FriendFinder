e0eb87d720af785dcabfa8e7fa11cf587b577a22
Tiana <wiggins.tiana@gmail.com>
2019-02-24 03:12:41 -0500

    fix errors

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("/Users/josephine-maye/Desktop/FriendFinder/app/routing/apiRoutes")(app);
 require("/Users/josephine-maye/Desktop/FriendFinder/app/routing/htmlRoutes")(app);
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 8080, function() {
 	console.log("App listening on PORT: " + PORT);
 });
